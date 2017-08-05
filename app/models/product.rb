08b33c9755ff950d493410e15fd099049577f16e
huazai <470021389@qq.com>
2017-08-05 22:44:38 +0800

    delere friendly id

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,30 +1,8 @@
 class Product < ApplicationRecord
   has_many :comments
-  belongs_to :category, :optional => true 
+  belongs_to :category, :optional => true
 
   mount_uploader :image, ImageUploader
 
   acts_as_votable
-
-  before_validation :generate_friendly_id, :on => :create
-
-
-  validates_presence_of :friendly_id
-
-  validates_uniqueness_of :friendly_id
-  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
-
-  def to_param
-    self.friendly_id
-  end
-
-  protected
-
-  def generate_friendly_id
-    self.friendly_id ||= SecureRandom.uuid
-  end
-
-
-
-
 end
